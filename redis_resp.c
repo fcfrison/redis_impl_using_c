f33e7450c4ea2e1f5b7c9bbaab4c86501f040e65
Felipe Frison <fcfrison@gmail.com>
2025-01-18 18:15:05 -0300

    refactor: improve login in get_arr_size.

--- redis_resp.c
+++ redis_resp.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <sys/socket.h>
 #define MAX_ARRAY_SIZE (size_t)512
+#define ERR_INV_CHAR -2
+#define ERR_ARRAY_OVERFLOW -2
 typedef struct ArrElem ArrElem;
 typedef enum{
     UNDEF_DTYPE = -1,
@@ -11,6 +13,14 @@ typedef enum{
     BULK_STR    =  2,
     ARRAY       =  3
 } RedisDtype;
+typedef enum ArraySizeStates{
+    START_STATE        = 0,
+    DIGIT_STATE        = 1,
+    NEG_DIGIT_STATE_I  = 2,
+    NEG_DIGIT_STATE_II = 3,
+    END_STATE          = 4
+
+};
 struct ArrElem{
     void*      content;
     RedisDtype type;
@@ -69,39 +79,143 @@ ArrElem* parse_bulk_str(void){};
 
 int
 get_arr_size(int fd){
+    // It's possible *0\r\n  --> empty arrays
+    // It's possible *-1\r\n --> nil arrays
+    // It's not possible *\r\n   
     ssize_t ret_val;
     char* buf = (char*) calloc(MAX_ARRAY_SIZE, sizeof(char));
     unsigned int  arr_size, n_bytes = 0;
-    unsigned char is_digit, state   = 0;
-    while(1){
-        ret_val = recv(fd, &buf[n_bytes], 1, 0);
-        if(ret_val==-1){
+    unsigned char is_digit, state   = START_STATE;
+    unsigned char is_negative       = 0;
+    unsigned char should_continue   = 1;
+    while(should_continue){
+        if(n_bytes>MAX_ARRAY_SIZE){
             free(buf);
-            return -1;
+            return ERR_ARRAY_OVERFLOW;
         }
-        if(state==0){
+        ret_val = recv(fd, &buf[n_bytes], 1, 0);
+        if(ret_val==-1 || ret_val>1){
+            free(buf);
+            return ERR_INV_CHAR;
+        }
+        switch (state){
+        // START_STATE: reading the first char. It must be a digit or '-'
+        case START_STATE:
+            is_digit = (buf[0]>47 && buf[0]<58);
+            if(is_digit){
+                n_bytes++;
+                state=DIGIT_STATE;
+                break;
+            }
+            if(buf[0]='-'){
+                state=NEG_DIGIT_STATE_I;
+                is_negative = 1;
+                break;
+            }
+            free(buf);
+            return ERR_INV_CHAR;
+        // DIGIT_STATE: the first char was a valid digit (0,1,2,...). The next char could be a digit or '\r'
+        case DIGIT_STATE:
+            is_digit = (buf[n_bytes]>47 && buf[n_bytes]<58);
+                if(is_digit){
+                    n_bytes++;
+                    break;
+                }
+                if(buf[n_bytes]=='\r'){
+                    state=END_STATE;
+                    break;
+                }
+                free(buf);
+                return ERR_INV_CHAR;
+        // NEG_DIGIT_STATE_I: the first char was '-'. The next digit must the digit '1'
+        case NEG_DIGIT_STATE_I:
+            if(buf[n_bytes]!='1'){
+                free(buf);
+                return ERR_INV_CHAR;
+            }
+            n_bytes++;
+            state=NEG_DIGIT_STATE_II;
+            break;
+        // NEG_DIGIT_STATE_II: the first char was '-'. The second was 1. The next digit must the digit '\r'
+        case NEG_DIGIT_STATE_II:
+            if(buf[n_bytes]!='\r'){
+                free(buf);
+                return ERR_INV_CHAR;
+            }
+            state=END_STATE;
+            break;
+        // END_STATE: the previous char was '\r' and the next must be '\n'
+        case END_STATE:
+            if(buf[n_bytes]!='\n'){
+                    free(buf);
+                    return ERR_INV_CHAR;
+                }
+            should_continue = 0;
+            break;
+        }
+        /*
+        if(state==START_STATE){
+            is_digit = (buf[0]>47 && buf[0]<58);
+            if(is_digit){
+                n_bytes++;
+                state=DIGIT_STATE;
+                continue;
+            }
+            if(buf[0]='-'){
+                state=NEG_DIGIT_STATE_I;
+                is_negative = 1;
+                continue;
+            }
+            free(buf);
+            return ERR_INV_CHAR;
+        }
+        // DIGIT_STATE: the first char was a valid digit (0,1,2,...). The next char could be a digit or '\r'
+        if(state==DIGIT_STATE){
             is_digit = (buf[n_bytes]>47 && buf[n_bytes]<58);
             if(is_digit){
                 n_bytes++;
                 continue;
             }
             if(buf[n_bytes]=='\r'){
-                state = 1;
+                state=END_STATE;
                 continue;
             }
             free(buf);
-            return -1;
+            return ERR_INV_CHAR;
         }
-        if(buf[n_bytes]!='\n'){
-            free(buf);
-            return -1;
+        // NEG_DIGIT_STATE_I: the first char was '-'. The next digit must the digit '1'
+        if(state==NEG_DIGIT_STATE_I){
+            if(buf[n_bytes]!='1'){
+                free(buf);
+                return ERR_INV_CHAR;
+            }
+            n_bytes++;
+            state=NEG_DIGIT_STATE_II;
+            continue;
         }
-        buf[n_bytes] = '\0';
-        break;
+        
+        if(state==NEG_DIGIT_STATE_II){
+            if(buf[n_bytes]!='\r'){
+                free(buf);
+                return ERR_INV_CHAR;
+            }
+            state=END_STATE;
+            continue;
+        }
+        // NEG_DIGIT_STATE_II: the first char was '-'. The second was 1. The next digit must the digit '\r'
+        // state END_STATE: the previous char was '\r' and the next must be '\n'
+        if(state==END_STATE){
+            if(buf[n_bytes]!='\n'){
+                free(buf);
+                return ERR_INV_CHAR;
+            }
+            break;
+        }
+        */
     }
     arr_size = string_to_uint(buf);
     free(buf);
-    return arr_size;
+    return is_negative?(-1)*arr_size:arr_size;
     };
 char     get_next_char(int fd){return '$';};
 void     delete_array(ArrElem* el){};
